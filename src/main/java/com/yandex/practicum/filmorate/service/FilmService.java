0e62c9ea4d202a66472b125766151d5d5ab9af5d
Vladimir Sharapov <vladimir.sharapov@shoppertrak.com>
2022-07-26 23:46:30 -0500

    add private modifier for fields and injected it through constructor

--- src/main/java/com/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/com/yandex/practicum/filmorate/service/FilmService.java
@@ -3,7 +3,6 @@ package com.yandex.practicum.filmorate.service;
 import com.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
 import com.yandex.practicum.filmorate.exceptions.UserNotFoundException;
 import com.yandex.practicum.filmorate.model.Film;
-import com.yandex.practicum.filmorate.model.User;
 import com.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +11,12 @@ import java.util.List;
 
 @Service
 public class FilmService {
+    private InMemoryFilmStorage inMemoryFilmStorage;
 
     @Autowired
-    InMemoryFilmStorage inMemoryFilmStorage;
+    public FilmService(InMemoryFilmStorage inMemoryFilmStorage) {
+        this.inMemoryFilmStorage = inMemoryFilmStorage;
+    }
 
 
     public List<Film> findAll() {
