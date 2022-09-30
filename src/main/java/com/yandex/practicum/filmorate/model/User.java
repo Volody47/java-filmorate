2c64fd8f37d4df0adc0cd7df204fd7d0fe89482f
Vladimir Sharapov <vladimir.sharapov@shoppertrak.com>
2022-09-29 23:45:18 -0500

    add fields to handle new logic with Mpa and Genre

--- src/main/java/com/yandex/practicum/filmorate/model/User.java
+++ src/main/java/com/yandex/practicum/filmorate/model/User.java
@@ -1,13 +1,16 @@
 package com.yandex.practicum.filmorate.model;
 
-import lombok.Data;
+import lombok.*;
 import net.minidev.json.annotate.JsonIgnore;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
     private int id;
     private String email;
@@ -16,4 +19,14 @@ public class User {
     private LocalDate birthday;
     @JsonIgnore
     private Set<Integer> friendIds = new HashSet<>();
+
+    public User(int user_id, String user_email, String user_login, String user_full_name, LocalDate user_birthday) {
+        this.id = user_id;
+        this.email = user_email;
+        this.login = user_login;
+        this.name = user_full_name;
+        this.birthday = user_birthday;
+
+    }
+
 }
